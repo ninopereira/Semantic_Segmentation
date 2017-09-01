7681f38f764f561e781a7c85518cf37c2f9812df
Nino Pereira <ninopereira.pt@gmail.com>
2017-09-01 23:17:08 +0100

    feat: add optimize function

--- main.py
+++ main.py
@@ -105,7 +105,17 @@ def optimize(nn_last_layer, correct_label, learning_rate, num_classes):
     :return: Tuple of (logits, train_op, cross_entropy_loss)
     """
     # TODO: Implement function
-    return None, None, None
+
+
+    labels = tf.reshape(correct_label, (-1, num_classes))
+    logits = tf.reshape(nn_last_layer, (-1, num_classes))
+    # calculate loss
+    cross_entropy_loss = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits, labels))
+    # adam optimizer
+    trained = tf.train.AdamOptimizer(learning_rate).minimize(loss)
+
+    return logits, trained, cross_entropy_loss
+    
 tests.test_optimize(optimize)
 
 
